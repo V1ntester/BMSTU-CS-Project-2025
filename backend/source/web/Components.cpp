da03c99f5038527896d610432fdabb7f671dcb16
vita-minka <vvolozhenkova@gmail.com>
2025-05-26 22:32:00 +0300

    Give a try 4 files

--- backend/source/web/Components.cpp
+++ backend/source/web/Components.cpp
@@ -1,12 +1,13 @@
 #include "../storage/Manager.hpp"
-
 #include "../components/models/TaskModel.hpp"     
 #include "../components/controllers/TaskController.hpp" 
+#include "../components/models/UserModel.hpp"
 
 namespace Web
 {
     Storage::Manager storageManager;
 
+    Components::UserModel userModel(storageManager);
     Components::TaskModel taskModel(storageManager);            
     Components::TaskController taskController(taskModel);       
 } // namespace Web
